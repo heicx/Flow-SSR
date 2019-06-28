e61d0abdf16941536e1b3cb5e6d975f5498d38ea
heichenxuan <heicx_sudo@163.com>
2019-06-28 15:52:01 +0800

    feat: 调整菜单默认选中逻辑

--- components/header.vue
+++ components/header.vue
@@ -3,7 +3,7 @@
 		<div class="container clearfix">
       <router-link class="topbar-logo" to='/' tag='a'></router-link>
 			<div class="topbar-tabs">
-				<router-link v-for='(item, index) in list' :key='index' :class='{"active": item.alias.indexOf($route.name) > -1}' :to='item.link' tag='a'>{{item.name}}{{$route.test}}</router-link>
+				<router-link v-for='(item, index) in list' :key='index' :class='{"active": $route.name == item.alias}' :to='item.link' tag='a'>{{item.name}}{{$route.test}}</router-link>
         <a href="//www.flowclub.com/complaint" target="_blank" class="nuxt-link-active">媒体资源</a>
       </div>
 		</div>
@@ -36,12 +36,20 @@ export default {
 					link: '/about'
         },
         {
-					name: '招商加盟',
+					name: '省市代招商',
 					alias: 'investment',
 					link: '/investment'
 				},
+        {
+					name: '专卖店加盟',
+					alias: 'entityshop',
+					link: '/entityshop'
+				},
 			]
 		}
-	}
+  },
+  mounted () {
+    console.log(this.$route.name)
+  }
 }
 </script>
