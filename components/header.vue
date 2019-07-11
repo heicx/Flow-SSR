cda8f7b7aa32b94aa81441f3d0ccde167cc26d32
heichenxuan <heicx_sudo@163.com>
2019-07-11 14:37:22 +0800

    feat: update header items.

--- components/header.vue
+++ components/header.vue
@@ -3,7 +3,7 @@
 		<div class="container clearfix">
       <router-link class="topbar-logo" to='/' tag='a'></router-link>
 			<div class="topbar-tabs">
-				<router-link v-for='(item, index) in list' :key='index' :class='{"active": $route.name == item.alias}' :to='item.link' tag='a'>{{item.name}}{{$route.test}}</router-link>
+				<router-link v-for='(item, index) in tabList' :key='index' :class='{"active": item.alias.indexOf($route.name) > -1 }' :to='item.link' tag='a'>{{item.name}}{{$route.test}}</router-link>
         <a href="//www.flowclub.com/complaint" target="_blank" class="nuxt-link-active">媒体资源</a>
       </div>
 		</div>
@@ -14,42 +14,54 @@
 export default {
 	data() {
 		return {
-			list: [
+			// list: this.test
+		}
+  },
+  computed: {
+    tabList() {
+      const cnEvnStr = !!window.location.pathname.match(/^\/cn/) ? '/cn' : '';
+      let list = [
 				{
 					name: '首页',
-					alias: 'index',
+					alias: ['index', 'cn'],
 					link: '/'
 				},
 				{
 					name: '产品技术',
-					alias: ['tech', 'spec'],
+					alias: ['tech', 'spec', 'cn-tech', 'cn-spec'],
 					link: '/tech'
 				},
 				{
 					name: '在线商城',
-					alias: 'shop',
+					alias: ['shop', 'cn-shop'],
 					link: '/shop'
         },
         {
 					name: '关于我们',
-					alias: 'about',
+					alias: ['about', 'cn-about'],
 					link: '/about'
         },
         {
 					name: '省市代招商',
-					alias: 'investment',
+					alias: ['investment', 'cn-investment'],
 					link: '/investment'
 				},
         {
 					name: '专卖店加盟',
-					alias: 'entityshop',
+					alias: ['entityshop', 'cn-entityshop'],
 					link: '/entityshop'
 				},
-			]
-		}
+      ]
+
+      for (let oItem of list) {
+        oItem.link = `${cnEvnStr}${oItem.link}`;
+      }
+
+      return list;
+    }
   },
   mounted () {
-    console.log(this.$route.name)
+    // console.log(this.$route.name)
   }
 }
 </script>
